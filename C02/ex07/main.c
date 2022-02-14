3d571f913b3f05eddf5a2baae39e42053e65e661
SeungBeom Jang <seungbeom.dev@gmail.com>
2022-02-14 21:26:04 +0900

    Delete C02 directory

--- C02/ex07/main.c
+++
@@ -1,9 +0,0 @@
-#include "stdio.h"
-
-char    *ft_strupcase(char *str);
-
-int main(void)
-{
-    char    arr[] = "ACCSCS12313Ckjnasdksjda";
-    printf("%s\n", ft_strupcase(arr));
-}
--- C02/ex08/main.c
+++
@@ -1,9 +0,0 @@
-#include "stdio.h"
-
-char    *ft_strlowcase(char *str);
-
-int main(void)
-{
-    char    arr[] = "ACCSCS2131Ckjnasdksjda";
-    printf("%s\n", ft_strlowcase(arr));
-}
