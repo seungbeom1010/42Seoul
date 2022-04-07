4c0c59a78bd256c6bff59249c3eac753699c490a
seungbeom1010 <seungbeom.dev@gmail.com>
2022-04-07 23:14:39 +0900

    4/7

--- 42piscine/Shell01/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/passwd | grep -v '#' | awk 'NR%2==0' | cut -d ':' -f 1 | rev | sort -r | awk -v L1=$FT_LINE1 -v L2=$FT_LINE2 'NR >= L1 && NR <= L2' | xargs | sed 's/ /, /g' | sed  's/$/./g' | tr -d '\n'
