4c8234c56ac01afd752eb376fdc79bba63986dab
seungbeom1010 <seungbeom.dev@gmail.com>
2022-04-16 23:15:59 +0900

    4/16

--- 42/Libft/ft_strnstr.c
+++ 42/Libft/ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seunjang <seunjang@student.42.fr>          +#+  +:+       +#+        */
+/*   By: seungbeom <seungbeom@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 14:36:18 by seunjang          #+#    #+#             */
-/*   Updated: 2022/04/14 16:04:18 by seunjang         ###   ########.fr       */
+/*   Updated: 2022/04/16 15:49:19 by seungbeom        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,11 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t			index;
-	unsigned char	*tmp_h;
-	unsigned char	*tmp_n;
+	size_t	index;
 
-	tmp_h = (unsigned char *)haystack;
-	tmp_n = (unsigned char *)needle;
 	if (ft_strlen(needle) == 0)
 		return ((char *)haystack);
 	index = 0;
-	while (tmp_h[index] && index < len)
-	{
-		if (tmp_h[index] == tmp_n[index])
-		{
-			if (tmp_n[index + 1] == '\0')
-				return ((char *)(tmp_h + index - ft_strlen(needle)));
-			index++;
-		}		
-	}
-	return (0);
+	
+	
 }
