0afbe71c2f1ae7887e35fe494c3f2155174787ea
seungbeom1010 <seungbeom.dev@gmail.com>
2022-04-21 13:43:11 +0900

    4/21

--- 42/Libft/ft_memcpy.c
+++ 42/Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: seungbeom <seungbeom@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 16:46:42 by seungbeom         #+#    #+#             */
-/*   Updated: 2022/04/20 21:47:41 by seungbeom        ###   ########.fr       */
+/*   Updated: 2022/04/21 11:02:42 by seungbeom        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	index;
 
-	index = 0;
 	if (!dst && !src)
 		return (0);
 	index = 0;
