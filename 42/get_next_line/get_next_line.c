a444df1f42d6885b55940c36f31cdddb6e91d688
seungbeom1010 <seungbeom.dev@gmail.com>
2022-05-03 21:06:03 +0900

    5/3

--- 42/get_next_line/get_next_line.c
+++ 42/get_next_line/get_next_line.c
@@ -6,11 +6,20 @@
 /*   By: seunjang <seunjang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 13:49:13 by seunjang          #+#    #+#             */
-/*   Updated: 2022/05/03 13:49:28 by seunjang         ###   ########.fr       */
+/*   Updated: 2022/05/03 20:50:10 by seunjang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*get_next_line(int fd)
 {
+	char	buff[BUFF_SIZE];
+	int		temp_buff;
 
+	
+	while  ((temp_buff = read(fd, buff, sizeof(buff) - 1)) > 0)
+	{
+		buff[temp_buff] = '\0';
+		return (buff);
+	}
+	return (NULL);
 }
--- 42/get_next_line/get_next_line.h
+++ 42/get_next_line/get_next_line.h
@@ -6,7 +6,17 @@
 /*   By: seunjang <seunjang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 13:49:47 by seunjang          #+#    #+#             */
-/*   Updated: 2022/05/03 13:49:48 by seunjang         ###   ########.fr       */
+/*   Updated: 2022/05/03 20:11:07 by seunjang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUTTER_SIZE 1
+
+char	*get_next_line(int fd)
+
+
+
+#endif
